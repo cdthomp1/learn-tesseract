6e2f93a7dbf639f407d7d2e0357c149d9fa9c248
cdthomp1 <tho17015@byui.edu>
2020-08-18 23:23:19 -0600

    imgs

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const worker = createWorker();
   await worker.load();
   await worker.loadLanguage('eng');
   await worker.initialize('eng');
-  const { data: { text } } = await worker.recognize('https://i.etsystatic.com/18059081/r/il/6a8847/1555416942/il_1140xN.1555416942_k0nu.jpg');
+  const { data: { text } } = await worker.recognize('https://www.thirtyhandmadedays.com/wp-content/uploads/2014/07/recipes-in-recipe-book.jpg');
   console.log(text);
   await worker.terminate();
 })();
